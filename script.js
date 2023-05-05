859c2a636ad1ec651541183dc14e6376d5fb2626
Iulian Gabriel <Burciuiuliangabriel18@gmail.com>
2023-05-05 17:56:50 +0300

    finishing touches

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 
 class Calculator {
     currentNumber = 0
+    buttonValues = []
     constructor(){
     this.lastOperation = null;
     this.buttonsFunctionality()
     }
     buttonsFunctionality(){
     //querySelectorAll returns a NodeList, which is a collection of elements that doesn't have a "addEventListener" method
-         let buttons = document.querySelectorAll('.buttons');
+    let buttons = document.querySelectorAll('.buttons');
          //And because of that we loop through the NodeList and attach the event listener to each button individually.
          buttons.forEach((button) =>{
              button.addEventListener('click', this.displayNumbers.bind(this));
@@ -18,20 +19,28 @@ class Calculator {
         const clickedButtonValue = event.target.textContent;
         const inputField = document.querySelector('.input');
         let lastChar = inputField.value[inputField.value.length - 1];
-
+        let findX = inputField.value.includes("X")
         if(clickedButtonValue === "="){
-            if(this.lastOperation === "+"){
+            if(this.lastOperation === "-" && findX){
+                return this.multiply();
+            } else if (this.lastOperation === "+"){
                 return this.sum();
             } else if (this.lastOperation === "-"){
                 return this.substract();
             } else if (this.lastOperation === "X"){
                 return this.multiply();
-            } else if (this.lastOperation === "/"){
-                return this.divide();
+            } else if(this.lastOperation === "/"){
+                return this.divide()
             } else {
                 return;
             }
+        } else if(clickedButtonValue === "C"){
+            inputField.value = "";
+            this.currentNumber = 0;
+            this.lastOperation = null;
+            return;
         }
+
         if(lastChar === "X" && clickedButtonValue === "-"){
         lastChar = clickedButtonValue
         } else if ("+-X/.".includes(clickedButtonValue) && "+-X/.".includes(lastChar)) {
@@ -44,7 +53,7 @@ class Calculator {
         this.currentNumber += clickedButtonValue;
         inputField.value = inputField.value === '00' ? clickedButtonValue : inputField.value + clickedButtonValue;
         if(clickedButtonValue === "+" || clickedButtonValue === "-" || clickedButtonValue === "X" || clickedButtonValue === "/"){
-            this.lastOperation = clickedButtonValue
+            this.lastOperation = clickedButtonValue;
         }
 
         if ("+-X/".includes(clickedButtonValue)) {
@@ -74,7 +83,7 @@ class Calculator {
         for(let i = 1; i < numbers.length; i++){
             const num = parseFloat(numbers[i])
             if(!isNaN(num)){
-                result -= num
+                result -= num;
             }
         }
         if (result % 1 !== 0 && result.toString().split('.')[1].length > 4) {
@@ -86,11 +95,11 @@ class Calculator {
     multiply(){
         const inputField = document.querySelector('.input');
         const numbers = inputField.value.split('X');
-        let result = 1
+        let result = 1;
         for(let i = 0; i < numbers.length; i++){
-            const num = parseFloat(numbers[i])
+            const num = parseFloat(numbers[i]);
             if(!isNaN(num)){
-                result *= num
+                result *= num;
             }
         }
         if (result % 1 !== 0 && result.toString().split('.')[1].length > 4) {
@@ -102,19 +111,20 @@ class Calculator {
     divide(){
         const inputField = document.querySelector('.input');
         const numbers = inputField.value.split('/');
-        let result = numbers[0]
+        let result = numbers[0];
         for(let i = 1; i < numbers.length; i++){
-            const num = parseFloat(numbers[i])
+            const num = parseFloat(numbers[i]);
             if(!isNaN(num)){
-                result /= num
+                result /= num;
             }
         }
         if(result / 0){
-            inputField.value = "Infinity"
+            inputField.value = "Infinity";
         }
-        if (result % 1 !== 0 && result.toString().split('.')[0].length > 4) {
+
+        if (result % 1 !== 0 && result.toString().split('.')[1].length > 4) {
             result = result.toFixed(4);
-          }
+        } 
         inputField.value = result;
         this.currentNumber = result;
     }
